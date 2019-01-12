022bfce1721419a0988596d4c3f1f8961d462ec5
Roger Cruz <roger_r_cruz@@yahoo.com>
2019-01-12 16:34:58 -0500

    New script for randomly stopping and restarting a service

---
+++ README.md
@@ -0,0 +1,3 @@
+Copyright Roger Cruz (roger_r_cruz@@yahoo.com)
+
+RandomlyStartStopService - Randomly starts/stops a service.  Useful for fault checking of another program using the service
---
+++ RandomlyStartStopService/RandomlyStartStopService.ps1
@@ -0,0 +1,46 @@
+﻿#
+# Script.ps1
+#
+$service=$args[0]
+
+# Process must not run between min and max secs
+$doNotRunMin = 60
+$doNotRunMax = 300
+
+# Process must run between min and max secs
+$runMin = 120
+$runMax = 360
+
+$iteration = 1
+function StopSleepStart
+{
+	Write-Host "Stoping service $service.  Iteration: $iteration"
+	Stop-Service $service
+	$sleep = Get-Random -Minimum $doNotRunMin -Maximum $doNotRunMax
+	Write-Host "Will restart the service in $sleep seconds"
+	Start-Sleep -Seconds $sleep
+
+	Write-Host "Starting service $service.  Iteration: $iteration"
+	Start-Service $service
+	$sleep = Get-Random -Minimum $runMin -Maximum $runMax
+	Write-Host "Letting it run for $sleep seconds before stopping the service"
+	Start-Sleep -Seconds $sleep
+	$iteration++
+}
+
+function Main()
+{
+	if (!$service)
+	{
+			"Please specify name of service to start/stop at random times"
+	}
+	else
+	{
+		while ($true)
+		{
+			StopSleepStart
+		}
+	}
+}
+
+Main
