de6d52cbb8430a6cbea974c20252a39bdcb60a72
Roger Cruz <roger.cruz@citrix.com>
2020-07-15 23:57:08 -0400

    Learned about Get-PSCallStack which can be used for extracting filename and line number.  Added script GetFunctionAndLineNumber

---
+++ GetFunctionAndLineNumber/GetFunctionAndLineNumber.ps1
@@ -0,0 +1,20 @@
+﻿
+function Get-CallStack
+{ 
+    [CmdletBinding()] 
+    Param 
+    ( 
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipelineByPropertyName=$true)] 
+        [ValidateNotNullOrEmpty()] 
+        [string]$Message
+    )
+ 
+    $stack = (Get-PSCallStack)    
+    $cmd = $($stack[0].Command)
+    $scriptName = $stack[0].ScriptName
+    $lineNumber = $stack[0].ScriptLineNumber 
+    Write-Host "Cmd $cmd, $script name = $scriptName, line # = $linenumber"
+    $stack | Select-Object -Property *
+}
+Get-CallStack "Test"
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Copyright Roger Cruz (roger_r_cruz@@yahoo.com)
 
+AmIElevatedAdmin - Determines if running as elevated admin.
+
+GenerateGuid - Generates a GUID
+
+MyRunningDirectory - Determines the path a script is executing from.
+
 RandomlyStartStopService - Randomly starts/stops a service.  Useful for fault checking of another program using the service
